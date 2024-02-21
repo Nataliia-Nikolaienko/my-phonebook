d10c08bc1f60c68c05e23a37ee79c1a8af747e37
Nataliia-Nikolaienko <natalina_pretty@ukr.net>
2024-02-21 13:01:52 +0100

    update restrictedRoute

--- src/components/RestrictedRoute.js
+++ src/components/RestrictedRoute.js
@@ -1,7 +1,7 @@
 import { useAuth } from 'hooks';
 import { Navigate } from 'react-router-dom';
 
-/**
+/*
  * - If the route is restricted and the user is logged in, render a <Navigate> to redirectTo
  * - Otherwise render the component
  */
